47fc74a07fb78b9f7f7a22516a48a0223df723bf
Wesan <fpswesan@gmail.com>
2025-11-04 16:21:39 +0800

    LeetCode 3

---
+++ Leetcode/567_Permutation_in_String.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string str1 = "abc";
+    string str2 = "lecabee";
+    string str3 = "lecaabee";
+    cout << sol.checkInclusion(str1, str2) << endl;
+    cout << sol.checkInclusion(str1, str3) << endl;
+    return 0;
+}
